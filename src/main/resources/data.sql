acacdaff7cf0dbfd6eed9d2deb13923facd0a5d5
Yusuke Nagatani <yu.nagatani@softventure.co.jp>
2022-11-10 14:05:34 +0900

    [fix]サーバにデプロイした場合に、URIにディレクトリ名が入ることでページ表示時にリダイレクトループになっていたのを修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,14 +2,14 @@ INSERT INTO users(username, password, name, rolename) VALUES ('admin', '$2a$10$b
 INSERT INTO users(username, password, name, rolename) VALUES ('user' , '$2a$10$yyT1siJCep647RT/I7KjcuUB5noFVU6RBo0FUXUJX.hb2MIlWTbDe', 'user-name' , 'USER' );
 /* 以下のコードをファイルの末尾に追記 */
 /* 全Roleのアクセス許可*/
-INSERT INTO access_authorization(rolename, uri) VALUES ('*', '/loginForm');
-INSERT INTO access_authorization(rolename, uri) VALUES ('*',  '/accessDeniedPage');
-INSERT INTO access_authorization(rolename, uri) VALUES ('*', '/logout');
+INSERT INTO access_authorization(rolename, uri) VALUES ('*', '/SmartEC_test/loginForm');
+INSERT INTO access_authorization(rolename, uri) VALUES ('*',  '/SmartEC_test/accessDeniedPage');
+INSERT INTO access_authorization(rolename, uri) VALUES ('*', '/SmartEC_test/logout');
 
 /* ADMIN Roleのアクセス許可 */
-INSERT INTO access_authorization(rolename, uri) VALUES ('ADMIN', '/home');
-INSERT INTO access_authorization(rolename, uri) VALUES ('ADMIN', '/adminPage');
+INSERT INTO access_authorization(rolename, uri) VALUES ('ADMIN', '/SmartEC_test/home');
+INSERT INTO access_authorization(rolename, uri) VALUES ('ADMIN', '/SmartEC_test/adminPage');
 
 /* USER Roleのアクセス許可 */
-INSERT INTO access_authorization(rolename, uri) VALUES ('USER',  '/home');
-INSERT INTO access_authorization(rolename, uri) VALUES ('USER',  '/userPage');
+INSERT INTO access_authorization(rolename, uri) VALUES ('USER',  '/SmartEC_test/home');
+INSERT INTO access_authorization(rolename, uri) VALUES ('USER',  '/SmartEC_test/userPage');
